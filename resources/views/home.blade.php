ceaa0762635f07827dc8003df889762d475788cc
bakhodur.nazriev <bakhodur.nazriev@gmail.com>
2019-11-15 15:35:29 +0500

    Create video and home controllers

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Lafeum</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+
+    </head>
+    <body>
+
+        <div>
+            <ol>
+            <?php foreach ($names as $name) : ?>
+                <li><?= $name; ?></li>
+            <?php endforeach; ?>
+            </ol>
+        </div>
+
+    </body>
+</html>

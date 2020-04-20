293229c6c12a0e3d7cf551b51e8fcf1282b025ae
bakhodur.nazriev <bakhodur.nazriev@gmail.com>
2020-04-20 17:21:50 +0500

    Add draggable tree view for categories

--- resources/js/components/admin/Categories.vue
+++ resources/js/components/admin/Categories.vue
@@ -1,39 +1,48 @@
 <template>
     <v-container fluid>
         <v-row justify="center">
-            <v-card class="col-lg-12 col-xl-8 p-0">
-                <draggable v-model="categories" class="bigger-area">
-                    <transition-group name="list-complete">
-                        <div
-                            v-for="(category, i) in categories"
-                            :key="i"
-                            class="pa-3 mb-2 teal white--text"
-                            @dragend="dragged"
+            <v-card class="col-lg-12 col-xl-8 pa-4" v-if="categories">
+                <!-- <NestedCategories :categories="categories"/> -->
+                <Tree v-model="categories">
+                    <div
+                        :style="{ cursor: 'pointer' }"
+                        slot-scope="{ node, tree, path }"
+                    >
+                        <v-icon
+                            class="mr-2"
+                            v-if="node.children.length"
+                            @click="tree.toggleFold(node, path)"
+                            small
+                            color="black"
                         >
-                            {{ category.name }}
-                        </div>
-                    </transition-group>
-                </draggable>
-                <!-- <ul>
-                    <Category
-                        v-for="(category, i) in categories"
-                        :key="i"
-                        :category="category"
-                    />
-                </ul> -->
+                            {{ node.$folded ? "mdi-plus" : "mdi-minus" }}
+                        </v-icon>
+                        {{ node.name }}
+                    </div>
+                </Tree>
+
+                <v-card-actions>
+                    <v-btn
+                        color="green accent-4 white--text"
+                        @click="saveCategoryTree"
+                    >
+                        Сохранить
+                    </v-btn>
+                </v-card-actions>
             </v-card>
         </v-row>
     </v-container>
 </template>
 
 <script>
-import Category from "./Category";
-import draggable from "vuedraggable";
+import NestedCategories from "./NestedCategories";
+import "he-tree-vue/dist/he-tree-vue.css";
+import { Tree, Draggable, Fold, foldAll, getPureTreeData } from "he-tree-vue";
 
 export default {
     components: {
-        Category,
-        draggable
+        NestedCategories,
+        Tree: Tree.mixPlugins([Draggable, Fold])
     },
     data() {
         return {
@@ -48,11 +57,20 @@ export default {
             let categoryType = this.$route.meta.type;
             axios
                 .get("/api/categories?type=" + categoryType)
-                .then(({ data }) => (this.categories = data))
+                .then(({ data }) => this.setCategories(data))
                 .catch(e => console.log(e));
         },
-        dragged(){
-            console.log(arguments);
+        saveCategoryTree() {
+            axios
+                .put("/api/categories", {
+                    categories: getPureTreeData(this.categories)
+                })
+                .then(({ data }) => this.setCategories(data))
+                .catch(e => console.log(e));
+        },
+        setCategories(categories) {
+            this.categories = categories;
+            foldAll(this.categories);
         }
     },
     watch: {

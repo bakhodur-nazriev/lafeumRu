fad73c87ea05fea007d9e8492308f13caaaf0f61
bakhodur.nazriev <bakhodur.nazriev@gmail.com>
2020-04-22 18:14:48 +0500

    Bug fix

--- resources/views/shows/showVocabulary.blade.php
+++ resources/views/shows/showVocabulary.blade.php
@@ -2,35 +2,38 @@
 
 @section("content")
     <div class="container">
-        <div class="row card-block my-2">
-            <div class="card-block">
-                <div class="row">
-                    <div class="col py-0 text-right">
-                        {{--<i data-id="{{$term->id}}"
-                           class="fa fa-star favourite-quote-btn {{$term->isFavorited() ? " fa-star-active": ""}}"
-                           data-toggle="tooltip"
-                           data-placement="top"
-                           title="Избранный"
-                        ></i>--}}
-                    </div>
+        <div class="m-0 mt-3 card-block">
+            <div class="row px-3">
+                <div class="py-0">
+                    <span class="mb-1">
+                        <i class="fa fa-diamond secondary"></i>
+                        <i class="fa fa-diamond secondary"></i>
+                        <i class="fa fa-diamond secondary"></i>
+                    </span>
                 </div>
-                <p class="mb-1">
-                    <i class="fa fa-diamond secondary"></i>
-                    <i class="fa fa-diamond secondary"></i>
-                    <i class="fa fa-diamond secondary"></i>
-                </p>
-                <p>{!! $term->body !!}</p>
-                <div class="tags-block">
-                    @foreach($term->categories as $category)
-                        <a href="#">
-                            <i class="fa fa-tags"></i>
-                            {{ $category->name }}
-                        </a>
-                    @endforeach
+                <div class="col p-0 text-right">
+                    <i data-id="{{$vocabulary->id}}"
+                       class="fa fa-star favourite-quote-btn {{$vocabulary->isFavorited() ? " fa-star-active": ""}}"
+                       data-toggle="tooltip"
+                       data-placement="top"
+                       title="Избранный"
+                    ></i>
                 </div>
-                <div class="my-main-divider"></div>
-                <button class="btn share-button">Поделиться</button>
             </div>
+            <p>{!! $vocabulary->body !!}</p>
+            <div class="tags-block">
+                @foreach($vocabulary->categories as $category)
+                    <a href="#">
+                        <i class="fa fa-tags"></i>
+                        {{ $category->name }}
+                    </a>
+                @endforeach
+            </div>
+            <div class="my-main-divider"></div>
+            <a class="share-button">
+                <div class="share-text">Поделиться</div>
+                <div class="share"></div>
+            </a>
         </div>
     </div>
 @endsection

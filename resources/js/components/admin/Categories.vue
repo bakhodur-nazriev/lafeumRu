4eaf845c9811925e7c95a6fbd8671ebfbd88561e
bakhodur.nazriev <bakhodur.nazriev@gmail.com>
2020-04-22 18:11:36 +0500

    Implement draggable tree, add show for category, add category edit form

--- resources/js/components/admin/Categories.vue
+++ resources/js/components/admin/Categories.vue
@@ -5,23 +5,31 @@
                 <h2>Категории</h2>
                 <Tree v-model="categories">
                     <div
+                        class="category-tree-node"
                         :style="{ cursor: 'pointer' }"
                         slot-scope="{ node, tree, path }"
-                        @click="tree.toggleFold(node, path)"
+                        @click="categoryToShow = node"
                     >
-                        <v-icon
-                            class="mr-2"
+                        <v-btn
                             v-if="node.children.length"
-                            small
+                            icon
+                            x-small
                             color="black"
+                            @click.stop="tree.toggleFold(node, path)"
                         >
-                            {{ node.$folded ? "mdi-plus" : "mdi-minus" }}
-                        </v-icon>
-                        {{ node.name }}
+                            <v-icon small>{{
+                                node.$folded
+                                    ? "mdi-chevron-down"
+                                    : "mdi-chevron-up"
+                            }}</v-icon>
+                        </v-btn>
+                        <span>
+                            {{ node.name }}
+                        </span>
                     </div>
                 </Tree>
                 <div class="mt-3 d-flex">
-                    <v-spacer/>
+                    <v-spacer />
                     <v-btn
                         color="green accent-4 white--text"
                         @click="saveCategoryTree"
@@ -32,6 +40,73 @@
             </v-card>
             <v-progress-circular indeterminate color="primary" v-else />
         </v-row>
+        <v-dialog v-model="showDialog" width="600">
+            <v-card v-if="showDialog">
+                <v-card-title class="headline pr-2">
+                    {{ categoryToShow.name }}
+                    <v-spacer />
+                    <v-btn icon @click="categoryToEdit = {...categoryToShow}">
+                        <v-icon small>mdi-pencil</v-icon>
+                    </v-btn>
+                    <v-btn icon>
+                        <v-icon small>mdi-delete</v-icon>
+                    </v-btn>
+                </v-card-title>
+
+                <v-card-text>
+                    {{ categoryToShow.description }}
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        color="primary darken-1"
+                        text
+                        @click="categoryToShow = null"
+                    >
+                        Закрыть
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        <v-dialog v-model="showEditDialog" width="600">
+            <v-card v-if="showEditDialog">
+                <v-card-title class="headline pr-2">
+                    Изменение категории
+                </v-card-title>
+
+                <v-card-text>
+                    <v-text-field 
+                        label="Название"
+                        outlined
+                        v-model="categoryToEdit.name"
+                    />
+                    <v-textarea
+                        label="Описание"
+                        v-model="categoryToEdit.description"
+                        outlined
+                    />
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        color="green darken-1"
+                        text
+                        @click="categoryToEdit = null"
+                    >
+                        Сохранить
+                    </v-btn>
+                    <v-btn
+                        color="primary darken-1"
+                        text
+                        @click="categoryToEdit = null"
+                    >
+                        Отмена
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-container>
 </template>
 
@@ -46,10 +121,36 @@ export default {
     data() {
         return {
             categories: [],
+            categoryToShow: null,
+            categoryToEdit: null
         };
     },
     mounted() {
         this.loadCategories();
+    },
+    computed: {
+        showDialog: {
+            get() {
+                return this.categoryToShow;
+            },
+            set(v) {
+                if (!v) {
+                    this.categoryToShow = null;
+                }
+            }
+        },
+        showEditDialog: {
+            get() {
+                return this.categoryToEdit;
+            },
+            set(v) {
+                if (!v) {
+                    this.categoryToEdit = null;
+                } else {
+                    this.categoryToShow = null;
+                }
+            }
+        }
     },
     methods: {
         loadCategories() {
@@ -82,3 +183,12 @@ export default {
     }
 };
 </script>
+
+<style>
+.he-tree .tree-node {
+    padding: 0;
+}
+.category-tree-node {
+    padding: 5px;
+}
+</style>

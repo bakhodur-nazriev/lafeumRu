e058d6e6ac5fbd4a42abe687b92e9e23b13486af
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-05 12:47:29 +0500

    Refactor terms page

---
+++ resources/js/views/TermsDeleteDialog.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-dialog v-if="termToDelete" v-model="termToDelete" width="500">
+        <v-card class="pa-2">
+            <v-card-title class="pt-1 regular headline text-center"
+                >Вы действительно хотите удалить это термин ?
+            </v-card-title>
+            <v-card-actions class="justify-center">
+                <v-btn color="green darken-1" dark @click="termToDelete = false"
+                    >Нет
+                </v-btn>
+                <v-btn color="red darken-1" dark @click="deleteTerm()"
+                    >Да
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    props:{
+        value: Object
+    },
+    methods: {
+        deleteTerm() {
+            axios
+                .delete("/api/terms/" + this.termToDelete.id)
+                .then(res => {
+                    this.$emit('deleted');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    },
+    computed: {
+        termToDelete: {
+            get(){
+                return this.value;
+            },
+            set(v){
+                if(!v){
+                    this.$emit('input', null);
+                }
+            }
+        }
+    }
+};
+</script>

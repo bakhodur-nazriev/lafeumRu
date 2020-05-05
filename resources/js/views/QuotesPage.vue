aaf5eccab3fe405cdf656d227405a93c7ed5b37f
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-05 10:51:00 +0500

    Refactor quotes page

--- resources/js/views/QuotesPage.vue
+++ resources/js/views/QuotesPage.vue
@@ -76,156 +76,59 @@
                     fab
                     fixed
                     right
-                    @click="dialogAdd = !dialogAdd"
+                    @click="addQuote = !addQuote"
                 >
                     <v-icon>mdi-plus</v-icon>
                 </v-btn>
             </template>
             <span>Добавить цитату</span>
         </v-tooltip>
-        <!-- Add Item Dialog -->
-        <v-dialog v-model="dialogAdd" width="700">
-            <v-card>
-                <v-form @submit="addQuote" ref="createForm">
-                    <v-card-title class="primary white--text mb-5">
-                        Создать Цитату
-                    </v-card-title>
-                    <v-card-text>
-                        <v-select
-                            outlined
-                            :items="authors"
-                            item-value="id"
-                            item-text="name"
-                            label="Автор"
-                            :rules="requiredField"
-                            v-model="newQuote.author_id"
-                        />
-                        <v-select
-                            outlined
-                            :items="categories"
-                            multiple
-                            item-value="id"
-                            item-text="name"
-                            label="Категории"
-                            :rules="requiredField"
-                            v-model="newQuote.categories"
-                        />
-                        <wysiwyg-editor
-                            v-model="newQuote.body"
-                            label="Введите цитату здесь"
-                        />
-                    </v-card-text>
-                    <v-card-actions>
-                        <v-spacer />
-                        <v-btn dark color="green" type="submit">
-                            Сохранить
-                        </v-btn>
-                        <v-btn
-                            dark
-                            color="error"
-                            type="button"
-                            @click="() => (dialogAdd = false)"
-                        >
-                            Отмена
-                        </v-btn>
-                    </v-card-actions>
-                </v-form>
-            </v-card>
-        </v-dialog>
-        <!-- Delete Item Dialog -->
-        <v-dialog v-if="quoteToDelete" v-model="quoteToDelete" width="500">
-            <v-card class="pa-2">
-                <v-card-title class="pt-1 regular headline text-center"
-                    >Вы действительно хотите удалить эту цитату ?
-                </v-card-title>
-                <v-card-actions class="justify-center">
-                    <v-btn
-                        color="green darken-1"
-                        dark
-                        @click="quoteToDelete = null"
-                        >Нет</v-btn
-                    >
-                    <v-btn color="red darken-1" dark @click="deleteQuote()"
-                        >Да</v-btn
-                    >
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-        <!-- Update Item Dialog -->
-        <v-dialog v-if="quoteToUpdate" v-model="quoteToUpdate" width="700">
-            <v-card>
-                <v-card-title class="primary white--text">
-                    Изменить Цитату
-                </v-card-title>
-                <v-container>
-                    <v-row justify="center">
-                        <v-col cols="12">
-                            <v-select
-                                hide-details
-                                outlined
-                                item-value="id"
-                                item-text="name"
-                                label="Авторы"
-                                :items="quotes.author_id"
-                                v-model="quoteToUpdate.author_id"
-                            >
-                            </v-select>
-                        </v-col>
-                        <v-col cols="12">
-                            <v-select
-                                hide-details
-                                outlined
-                                multiple
-                                item-value="id"
-                                item-text="name"
-                                label="Категории"
-                                :items="quotes.categories"
-                                v-model="quoteToUpdate.categories"
-                            >
-                            </v-select>
-                        </v-col>
-                        <v-col cols="12">
-                            <wysiwyg-editor
-                                label="Изменить цитату"
-                                v-model="quoteToUpdate.body"
-                            />
-                        </v-col>
-                    </v-row>
-                </v-container>
-                <v-card-actions>
-                    <v-spacer />
-                    <v-btn dark color="green" @click="updateQuote()"
-                        >Сохранить</v-btn
-                    >
-                    <v-btn dark color="error" @click="quoteToUpdate = false"
-                        >Отмена</v-btn
-                    >
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+
+        <quotes-create-dialog
+            v-model="addQuote"
+            :authors="authors"
+            :categories="categories"
+            @created="quoteCreated"
+        />
+
+        <quotes-update-dialog
+            v-model="quoteToUpdate"
+            :authors="authors"
+            :categories="categories"
+            @updated="quoteUpdated"
+        />
+
+        <quotes-delete-dialog
+            v-model="quoteToDelete"
+            @deleted="quoteDeleted"
+        />
+
     </v-content>
 </template>
 
 <script>
 import WysiwygEditor from "../components/WysiwygEditor";
+import QuotesCreateDialog from "../views/QuotesCreateDialog";
+import QuotesUpdateDialog from "../views/QuotesUpdateDialog";
+import QuotesDeleteDialog from "../views/QuotesDeleteDialog";
 
 export default {
-    components: { 
-        'wysiwyg-editor': WysiwygEditor
+    components: {
+        "wysiwyg-editor": WysiwygEditor,
+        "quotes-create-dialog": QuotesCreateDialog,
+        "quotes-update-dialog": QuotesUpdateDialog,
+        "quotes-delete-dialog": QuotesDeleteDialog
     },
     data() {
         return {
             authors: [],
             categories: [],
-            newQuote: null,
             search: "",
+            addQuote: false,
             quoteToDelete: null,
             quoteToUpdate: null,
             page: 1,
             pageCount: 2,
-            dialogAdd: false,
-            dialogDelete: false,
-            dialogUpdate: false,
             itemsPerPage: 15,
             editedIndex: -1,
             indexIterator: null,
@@ -260,9 +163,6 @@ export default {
             ]
         };
     },
-    beforeMount(){
-        this.newQuote = this.getDefaultQuote();
-    },
     mounted() {
         this.loadAuthors();
         this.loadQuoteCategories();
@@ -294,60 +194,18 @@ export default {
                 .then(res => (this.categories = res.data))
                 .catch(e => console.log(e));
         },
-        getDefaultQuote() {
-            return {
-                body: "",
-                author_id: null,
-                categories: null
-            };
+        quoteCreated(newQuote){
+            this.addQuote = false;
+            this.loadQuotes();
         },
-        resetNewQuoteForm() {
-            this.newQuote = this.getDefaultQuote();
-            this.$refs.createForm.reset();
-            this.dialogAdd = false;
+        quoteUpdated(updated){
+            this.quoteToUpdate = null;
+            this.loadQuotes();
         },
-        addQuote(e) {
-            e.preventDefault();
-
-            const validForm = this.$refs.createForm.validate();
-
-            if (!validForm) return;
-
-            axios
-                .post("/api/quotes/", this.newQuote)
-                .then(res => {
-                    this.resetNewQuoteForm();
-                    this.loadQuotes();        
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+        quoteDeleted(){
+            this.quoteToDelete = null;
+            this.loadQuotes();
         },
-        updateQuote() {
-            axios
-                .put("/api/quotes/" + this.quoteToUpdate.id, {
-                    body: this.quoteToUpdate.body,
-                    author_id: this.quoteToUpdate.author_id
-                })
-                .then(res => {
-                    this.quoteToUpdate = false;
-                    this.loadQuotes();
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        },
-        deleteQuote() {
-            axios
-                .delete("/api/quotes/" + this.quoteToDelete.id)
-                .then(res => {
-                    this.loadQuotes();
-                    this.quoteToDelete = false;
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        }
     },
     computed: {
         filteredQuotes() {
@@ -356,21 +214,6 @@ export default {
                     .toLowerCase()
                     .includes(this.search.toLowerCase());
             });
-        },
-        requiredField() {
-            return [
-                v => {
-                    if (Array.isArray(v) && v.length == 0) {
-                        return "Обязательное поле";
-                    }
-
-                    if (!v) {
-                        return "Обязательное поле";
-                    }
-
-                    return true;
-                }
-            ];
         }
     }
 };

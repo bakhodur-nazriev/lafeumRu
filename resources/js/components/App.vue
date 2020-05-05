8be07445b656ca14aea62be9056c5201e13d4636
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-05 18:01:22 +0500

    Change style of default scroll bar

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,68 +1,82 @@
 <template>
     <v-app class="grey lighten-4">
-        <navbar/>
+        <navbar />
 
         <v-content class="mx-4 my-2">
-            <router-view/>
+            <router-view />
         </v-content>
     </v-app>
 </template>
 
 <script>
-    import navbar from "./layouts/TheNavbar";
+import navbar from "./layouts/TheNavbar";
 
-    export default {
-        props: ["users"],
-        name: "App",
-        components: {navbar},
-        data() {
-            return {};
-        },
-    };
+export default {
+    props: ["users"],
+    name: "App",
+    components: { navbar },
+    data() {
+        return {};
+    }
+};
 </script>
 
 <style>
-    .v-application p {
-        margin-bottom: 0;
-    }
+*::-webkit-scrollbar-track {
+    border-radius: 10px;
+    background-color: #d9d9d9;
+}
 
-    .theme--light.v-data-table thead tr:last-child th {
-        font-size: 15px;
-        font-weight: bold;
-    }
+*::-webkit-scrollbar {
+    width: 8px;
+    background-color: transparent;
+}
 
-    .v-application
-    .error--text {
-        outline: none;
-    }
+*::-webkit-scrollbar-thumb {
+    border-radius: 50px;
+    background-color: #ababab;
+}
+*::-webkit-scrollbar-thumb:hover {
+    background-color: #777;
+}
+.v-application p {
+    margin-bottom: 0;
+}
 
-    .v-application
-    .primary--text {
-        outline: none;
-    }
+.theme--light.v-data-table thead tr:last-child th {
+    font-size: 15px;
+    font-weight: bold;
+}
 
-    .v-application
-    .primary {
-        outline: none;
-    }
+.v-application .error--text {
+    outline: none;
+}
 
-    .short-paragraph p {
-        white-space: nowrap;
-        overflow: hidden;
-        padding: 5px;
-        text-overflow: ellipsis;
-        width: 52vw;
-    }
+.v-application .primary--text {
+    outline: none;
+}
 
-    .mine-table-headers {
-        font-size: 18px;
-    }
+.v-application .primary {
+    outline: none;
+}
 
-    .tiptap-vuetify-editor__toolbar .v-toolbar {
-        justify-content: center;
-    }
+.short-paragraph p {
+    white-space: nowrap;
+    overflow: hidden;
+    padding: 5px;
+    text-overflow: ellipsis;
+    width: 52vw;
+}
 
-    :focus {
-        outline: none !important;
-    }
+.mine-table-headers {
+    font-size: 18px;
+}
+
+.tiptap-vuetify-editor__toolbar .v-toolbar {
+    justify-content: center;
+}
+
+:focus {
+    outline: none !important;
+}
 </style>

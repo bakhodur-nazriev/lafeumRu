ad5a3147eb6af05e91f817ffd041dd27de2fa940
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-05 17:24:00 +0500

    Refactor videos page

---
+++ resources/js/views/VideosDeleteDialog.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-dialog v-if="videoToDelete" v-model="videoToDelete" width="500">
+        <v-card class="pa-2">
+            <v-card-title class="pt-1 regular headline text-center">
+                Вы действительно хотите удалить это видео ?
+            </v-card-title>
+            <v-card-actions class="justify-center">
+                <v-btn
+                    color="green darken-1"
+                    dark
+                    @click="$emit('input', null)"
+                >
+                    Нет
+                </v-btn>
+                <v-btn color="red darken-1" dark @click="deleteVideo()"
+                    >Да</v-btn
+                >
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    props: {
+        value: Object
+    },
+    methods: {
+        deleteVideo() {
+            axios
+                .delete("/api/videos/" + this.videoToDelete.id)
+                .then(res => {
+                    this.$emit('deleted')
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    },
+    computed: {
+        videoToDelete: {
+            get(){
+                return this.value;
+            },
+            set(v){
+                if(!v){
+                    this.$emit('input', null);
+                }
+            }
+        }
+    }
+};
+</script>

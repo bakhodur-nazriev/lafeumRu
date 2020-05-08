fc16ed2d4850f604fb0dd687814f9532b738a853
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-08 12:42:36 +0500

    Refactor quotes page

--- resources/js/views/QuotesPage.vue
+++ resources/js/views/QuotesPage.vue
@@ -1,88 +1,22 @@
 <template>
     <v-content class="pa-0">
-        <v-container fluid>
-            <v-row justify="center">
-                <v-col md="6" xl="4">
-                    <v-text-field
-                        solo
-                        hide-details
-                        class="mb-1"
-                        label="Поиск"
-                        append-icon="mdi-magnify"
-                        v-model="search"
-                    >
-                    </v-text-field>
-                </v-col>
-                <v-col cols="12">
-                    <v-data-table
-                        :headers="headers"
-                        :items="filteredQuotes"
-                        :items-per-page="itemsPerPage"
-                        :page.sync="page"
-                        @page-count="pageCount = $event"
-                        hide-default-footer
-                        class="elevation-2"
-                        :loading="loadingQuotes"
-                        loading-text="Загрузка..."
-                    >
-                        <template v-slot:item.action="{ item }">
-                            <v-btn
-                                fab
-                                dark
-                                small
-                                color="primary"
-                                elevation="2"
-                                outlined
-                                @click="quoteToUpdate = item"
-                            >
-                                <v-icon dark>mdi-pen</v-icon>
-                            </v-btn>
-                            <v-btn
-                                fab
-                                dark
-                                small
-                                color="error"
-                                elevation="2"
-                                outlined
-                                @click="quoteToDelete = item"
-                            >
-                                <v-icon dark>mdi-delete</v-icon>
-                            </v-btn>
-                        </template>
-                        <template v-slot:item.body="{ item }">
-                            <div
-                                v-html="item.body"
-                                class="short-paragraph my-3 three-line-truncate"
-                            />
-                        </template>
-                    </v-data-table>
-                    <v-col class="text-center mt-2">
-                        <v-pagination
-                            :total-visible="7"
-                            v-model="page"
-                            :length="pageCount"
-                        ></v-pagination>
-                    </v-col>
-                </v-col>
-            </v-row>
-        </v-container>
-        <v-tooltip top>
-            <template v-slot:activator="{ on }">
-                <v-btn
-                    bottom
-                    color="primary"
-                    v-on="on"
-                    dark
-                    fab
-                    fixed
-                    right
-                    @click="addQuote = !addQuote"
-                >
-                    <v-icon>mdi-plus</v-icon>
-                </v-btn>
+        <index-page-layout
+            index-url="/api/quotes"
+            :table-headers="this.headers"
+            add-label="Добавить цитату"
+            search-field="body"
+            ref="indexPage"
+            @add-item="addQuote = true"
+            @update-item="quoteToUpdate = $event"
+            @delete-item="quoteToDelete = $event"
+        >
+            <template v-slot:item.body="{ item }">
+                <div
+                    v-html="item.body"
+                    class="short-paragraph my-3 three-line-truncate"
+                />
             </template>
-            <span>Добавить цитату</span>
-        </v-tooltip>
+        </index-page-layout>
 
         <quotes-create-dialog
             v-model="addQuote"
@@ -98,21 +32,19 @@
             @updated="quoteUpdated"
         />
 
-        <quotes-delete-dialog
-            v-model="quoteToDelete"
-            @deleted="quoteDeleted"
-        />
-
+        <quotes-delete-dialog v-model="quoteToDelete" @deleted="quoteDeleted" />
     </v-content>
 </template>
 
 <script>
+import IndexPageLayout from "../components/IndexPageLayout";
 import QuotesCreateDialog from "../views/QuotesCreateDialog";
 import QuotesEditDialog from "../views/QuotesEditDialog";
 import QuotesDeleteDialog from "../views/QuotesDeleteDialog";
 
 export default {
     components: {
+        IndexPageLayout,
         QuotesCreateDialog,
         QuotesEditDialog,
         QuotesDeleteDialog
@@ -121,17 +53,9 @@ export default {
         return {
             authors: [],
             categories: [],
-            search: "",
             addQuote: false,
             quoteToDelete: null,
             quoteToUpdate: null,
-            page: 1,
-            pageCount: 2,
-            itemsPerPage: 15,
-            editedIndex: -1,
-            indexIterator: null,
-            quotes: [],
-            loadingQuotes: false,
             headers: [
                 {
                     text: "Цитаты",
@@ -144,13 +68,6 @@ export default {
                     value: "author.name",
                     align: "center",
                     sortable: false
-                },
-                {
-                    text: "Действия",
-                    value: "action",
-                    align: "center",
-                    sortable: false,
-                    width: "160px"
                 }
             ]
         };
@@ -158,22 +75,8 @@ export default {
     mounted() {
         this.loadAuthors();
         this.loadQuoteCategories();
-        this.loadQuotes();
     },
     methods: {
-        loadQuotes() {
-            this.loadingQuotes = true;
-            axios
-                .get("/api/quotes")
-                .then(res => {
-                    this.loadingQuotes = false;
-                    this.quotes = res.data;
-                })
-                .catch(err => {
-                    this.loadingPhotos = false;
-                    console.log(err);
-                });
-        },
         loadAuthors() {
             axios
                 .get("/api/authors")
@@ -186,26 +89,17 @@ export default {
                 .then(res => (this.categories = res.data))
                 .catch(e => console.log(e));
         },
-        quoteCreated(newQuote){
+        quoteCreated(newQuote) {
             this.addQuote = false;
-            this.loadQuotes();
+            this.$refs.indexPage.loadItems();
         },
-        quoteUpdated(updated){
+        quoteUpdated(updated) {
             this.quoteToUpdate = null;
-            this.loadQuotes();
+            this.$refs.indexPage.loadItems();
         },
-        quoteDeleted(){
+        quoteDeleted() {
             this.quoteToDelete = null;
-            this.loadQuotes();
-        },
-    },
-    computed: {
-        filteredQuotes() {
-            return this.quotes.filter(quote => {
-                return quote.body
-                    .toLowerCase()
-                    .includes(this.search.toLowerCase());
-            });
+            this.$refs.indexPage.loadItems();
         }
     }
 };

68901d64ad5288a437a326d4bf0e7a1be548622c
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-12 11:34:02 +0500

    Implement categories show

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Quote;
+use App\Term;
+use App\Video;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->authorizeResource(Category::class);
     }
 
@@ -38,25 +42,43 @@ class CategoriesController extends Controller
         return $request->categories;
     }
 
+    public function showQuotes($categorySlug)
+    {
+        $category = $this->getCategory(Quote::class, $categorySlug);
+        return view('shows.category', compact('category'));
+    }
+
+    public function showTerms($categorySlug)
+    {
+        $category = $this->getCategory(Term::class, $categorySlug);
+        return view('shows.category', compact('category'));
+    }
+
+    public function showVideos($categorySlug)
+    {
+        $category = $this->getCategory(Video::class, $categorySlug);
+        return view('shows.category', compact('category'));
+    }
+
     public function store(Request $request)
     {
         $request->validate([
             'name' => 'required',
             'type' => 'required'
         ]);
-        
+
         $newCategory = new Category(
             $request->only(['name', 'description', 'type'])
         );
-        
+
         $parent = Category::find($request->parentId);
-        
+
         $newCategory->save();
 
         if ($parent) {
             $newCategory->appendToNode($parent)->save();
         }
-        
+
         return $newCategory;
     }
 
@@ -70,5 +92,50 @@ class CategoriesController extends Controller
     public function destroy(Category $category)
     {
         $category->delete();
+    }
+
+    /**
+     * Helpers
+     */
+
+    private function getCategory($categoriable, $slug)
+    {
+        $category = Category::where('type', $categoriable)->where('slug', $slug)->first();
+
+        $category->categoriables = $this->getCategoriables($categoriable, $category);
+        
+        return $category;
+    }
+
+    private function getCategoriables($model, $category)
+    {
+        $categoryDescendantIds = $category->descendants()->pluck('id');
+
+        $categoryIds = $categoryDescendantIds;
+
+        $categoryIds[] = $category;
+
+        $categoriableQuery = $model::whereHas('categories', function ($query) use ($categoryIds) {
+            $query->whereIn('id', $categoryIds);
+        });
+
+        $this->addCategoriableRelations($categoriableQuery, $model);
+
+        return $categoriableQuery->paginate(10);
+    }
+
+    private function addCategoriableRelations($categoriableQuery, $model)
+    {
+        switch ($model) {
+            case Quote::class:
+                $categoriableQuery->with('author:id,name,slug', 'categories:id,name,slug');
+                break;
+            case Term::class:
+                $categoriableQuery->with('knowledge:id,name,slug', 'categories:id,name,slug');
+                break;
+            case Video::class:
+                $categoriableQuery->with('channel:id,name,slug', 'categories:id,name,slug');
+                break;
+        }
     }
 }
---
+++ resources/views/shows/category.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.default')
+
+@section('content')
+<div class="row card-main-block">
+    @include('layouts.categories', ['type' => $category->type])
+    <div class="col-md-9 col-xl-9 col-sm-12 d-flex justify-content-around flex-wrap">
+        <div class="col-md-8 col-lg-8">
+            <h3 class="secondary">{{$category->name}}</h3>
+            <p>{{$category->description}}</p>
+
+            @switch($category->type)
+                @case('App\\Quote')
+
+                    @foreach ($category->categoriables as $quote)
+                        @include('layouts.quoteItem')
+                    @endforeach
+
+                @break
+                @case('App\\Term')
+
+                    @foreach ($category->categoriables as $term)
+                        @include('layouts.termItem')
+                    @endforeach
+
+                @break
+                @case('App\\Video')
+
+                    @foreach ($category->categoriables as $video)
+                        @include('layouts.videoItem')
+                    @endforeach
+
+                @break
+            @endswitch
+
+            <div class="row">
+                <div class="col-12">
+                    <nav aria-label="Page navigation example">
+                        <div class="row d-flex justify-content-center mt-3">
+                            {{ $category->categoriables->links() }}
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        @include('layouts.rightSidebar')
+    </div>
+</div>
+@endsection

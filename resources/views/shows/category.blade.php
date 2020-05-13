1a8ad3e4a1442847948a789564a73936586e0635
bakhodur.nazriev <bakhodur.nazriev@gmail.com>
2020-05-13 11:38:44 +0500

    fix category blade right sidebar

--- resources/views/shows/category.blade.php
+++ resources/views/shows/category.blade.php
@@ -1,48 +1,51 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="row card-main-block">
-    @include('layouts.categories', ['type' => $category->type, 'active' => $category->id])
-    <div class="col-md-9 col-xl-9 col-sm-12 d-flex justify-content-around flex-wrap">
-        <div class="col-md-8 col-lg-8">
-            <h3 class="secondary">{{$category->name}}</h3>
-            <p>{{$category->description}}</p>
+    <div class="row card-main-block">
+        @include('layouts.categories', ['type' => $category->type, 'active' => $category->id])
+        <div class="col-md-9 col-xl-9 col-sm-12 d-flex justify-content-between flex-wrap">
+            <div class="col-md-7 col-lg-7">
+                <h3 class="secondary">{{$category->name}}</h3>
+                <p>{{$category->description}}</p>
 
-            @switch($category->type)
-                @case('App\\Quote')
+                @switch($category->type)
+                    @case('App\\Quote')
 
                     @foreach ($category->categoriables as $quote)
                         @include('layouts.quoteItem')
                     @endforeach
 
-                @break
-                @case('App\\Term')
+                    @break
+                    @case('App\\Term')
 
                     @foreach ($category->categoriables as $term)
                         @include('layouts.termItem')
                     @endforeach
 
-                @break
-                @case('App\\Video')
+                    @break
+                    @case('App\\Video')
 
                     @foreach ($category->categoriables as $video)
                         @include('layouts.videoItem')
                     @endforeach
 
-                @break
-            @endswitch
+                    @break
+                @endswitch
 
-            <div class="row">
-                <div class="col-12">
-                    <nav aria-label="Page navigation example">
-                        <div class="row d-flex justify-content-center mt-3">
-                            {{ $category->categoriables->links() }}
-                        </div>
-                    </nav>
+                <div class="row">
+                    <div class="col-12">
+                        <nav aria-label="Page navigation example">
+                            <div class="row d-flex justify-content-center mt-3">
+                                {{ $category->categoriables->links() }}
+                            </div>
+                        </nav>
+                    </div>
                 </div>
             </div>
+            <div class="col-md-12 col-lg-4 d-flex flex-column">
+                @include('layouts.rightSidebarUserBlock')
+                @include('layouts.postsSidebarPostsBlock')
+            </div>
         </div>
-        @include('layouts.rightSidebar')
     </div>
-</div>
 @endsection

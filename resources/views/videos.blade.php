fab1f5c01447198d51e238fcdd601517882dc841
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-14 10:40:25 +0500

    Fix some styling

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -1,27 +1,28 @@
 @extends('layouts.default')
 
 @section('content')
-    <div class="row card-main-block">
-        @include('layouts.categories', ['type' => 'App\Video'])
-        <div class="col-xl-9 d-flex">
-            <div class="col-md-8 p-0">
-                <h3 class="secondary ml-3">Видео</h3>
+<div class="row card-main-block">
+    @include('layouts.categories', ['type' => 'App\Video'])
+    <div class="col-xl-9 d-flex">
+        <div class="col-md-8">
+            <h3 class="secondary">Видео</h3>
+            <div class="row">
                 @foreach($videos as $video)
                     @include('layouts.videoItem')
                 @endforeach
-                <div class="col-12">
-                    <nav aria-label="Page navigation example">
-                        <div class="row d-flex justify-content-center mt-3">
-                            {{ $videos->links() }}
-                        </div>
-                    </nav>
-                </div>
             </div>
-            <div class="col-md-12 col-lg-4 d-flex flex-column">
-                @include('layouts.rightSidebarUserBlock')
-                @include('layouts.postsSidebarPostsBlock')
+            <div class="col-12">
+                <nav aria-label="Page navigation example">
+                    <div class="row d-flex justify-content-center mt-3">
+                        {{ $videos->links() }}
+                    </div>
+                </nav>
             </div>
         </div>
+        <div class="col-md-12 col-lg-4 d-flex flex-column">
+            @include('layouts.rightSidebarUserBlock')
+            @include('layouts.postsSidebarPostsBlock')
+        </div>
     </div>
+</div>
 @endsection
-

2dfd451c656f6dd280e01e19e0cc690e1302c451
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-16 19:35:04 +0500

    Add post

---
+++ app/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public $timestamps = false;
+
+    protected $with = ['postable'];
+
+    public function postable()
+    {
+        return $this->morphTo();
+    }    
+}

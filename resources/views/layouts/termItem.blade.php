fa411994903b6a7f1928e50e631386e901c36435
bakhodur.nazriev <bakhodur.nazriev@gmail.com>
2020-05-18 10:40:33 +0500

    Fix termItem icon card block

--- resources/views/layouts/termItem.blade.php
+++ resources/views/layouts/termItem.blade.php
@@ -1,9 +1,9 @@
 <div class="card-block">
     <div class="row px-3 mb-2 d-flex">
         <div class="col-6 p-0 atom-icon-block">
-            <img src="img/icons/atom.png"/>
-            <img src="img/icons/atom.png"/>
-            <img src="img/icons/atom.png"/>
+            <img src="/img/icons/atom.png"/>
+            <img src="/img/icons/atom.png"/>
+            <img src="/img/icons/atom.png"/>
         </div>
         <div class="col-6 p-0 text-right">
             <i data-id="{{$term->id}}"

eb04a9072ee7988bd0456245ee9e02c45dc3216a
bakhodur.nazriev <bakhodur.nazriev@gmail.com>
2020-05-19 12:03:32 +0500

    Refactor count items from home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,42 +56,8 @@
                     </div>
                 @endforeach
             </div>
-            <div class="row my-4">
-                <div class="col text-center text-uppercase">
-                    <div class="px-4 py-3" style="background-color: #04718c; color: white">посетителей</div>
-                    <div class="px-4 py-3" style="background-color: #e0e0e0; font-size: 24px; font-weight: bold;">150
-                    </div>
-                </div>
-                <div class="col text-center text-uppercase">
-                    <div class="px-4 py-3" style="background-color: #04718c; color: white">стран</div>
-                    <div class="px-4 py-3" style="background-color: #e0e0e0; font-size: 24px; font-weight: bold;">50
-                    </div>
-                </div>
-                <div class="col text-center text-uppercase">
-                    <div class="px-4 py-3" style="background-color: #04718c; color: white">цитат</div>
-                    <div class="px-4 py-3"
-                         style="background-color: #e0e0e0; font-size: 24px; font-weight: bold;">{{$countOfFavoritesQuotes}}</div>
-                </div>
-                <div class="col text-center text-uppercase">
-                    <div class="px-4 py-3" style="background-color: #04718c; color: white">авторов</div>
-                    <div class="px-4 py-3"
-                         style="background-color: #e0e0e0; font-size: 24px; font-weight: bold;">{{$countOfAuthors}}
-                    </div>
-                </div>
-                <div class="col text-center text-uppercase">
-                    <div class="px-4 py-3" style="background-color: #04718c; color: white">терминов</div>
-                    <div class="px-4 py-3"
-                         style="background-color: #e0e0e0; font-size: 24px; font-weight: bold;">{{$countOfFavoritesTerms}}
-                    </div>
-                </div>
-                <div class="col text-center text-uppercase">
-                    <div class="px-4 py-3" style="background-color: #04718c; color: white">медиаконтент</div>
-                    <div class="px-4 py-3"
-                         style="background-color: #e0e0e0; font-size: 24px; font-weight: bold;">{{ $countOfMediaContent }}
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
     @include('layouts.right-sidebar.rightSidebar')
+    {{--@include('layouts.countItems')--}}
 @endsection

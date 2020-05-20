f859189b6f1adb91e735673f98a86bcb75678f03
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-20 16:02:37 +0500

    Refactor home layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,63 +1,68 @@
-@extends('layouts.default')
+@extends('layouts.frontApp')
 
-@section('content')
-    <div class="container">
-        <div class="row justify-center">
-            <div class="col-lg-11 offset-1">
-                <div class="col text-center about-us-section">
-                    <h3 class="secondary">О Сайте</h3>
-                    <img src="/img/lafeum-main-logo.png" alt="Logo lafeum">
-                    <p>Информированность о методах развития личности и совершенствования профессиональных знаний,
-                        С этой целью и создан данный ресурс, который посвящен самореализации. Здесь собраны,
-                        соответствующие
-                        тематике сайта,
-                        цитаты и афоризмы, термины научного мира и полезные ссылки на познавательно-образовательные
-                        каналы
-                        YouTube.
-                        Для удобства навигации и чтения, темы сайта разделены на четыре взаимосвязанные группы.
-                        Основным источником терминов (более 98%) является Википедия. В каждом термине есть ссылка на
-                        этот
-                        популярный ресурс,
-                        где вы можете более детально получить желаемую информацию. Комментарии и мысли специалистов
-                        взяты с
-                        популярных сайтов,
-                        на которые также указаны источники для подробного чтения.<br>
-                        Приятного и полезного чтения!</p>
-                </div>
-                <div class="col-xl-12">
-                    <h3 class="secondary text-center">Темы</h3>
-                </div>
-                <div class="row">
-                    @foreach($categories as $category)
-                        <div class="col-lg-3 align-top mb-3">
-                            <div class="categories-main-name">
-                                <a href="#"><b>{{ $category->name }}</b></a>
-                            </div>
-                            @foreach($category->children as $subCategory)
+@section('layout')
+
+    @include('layouts.navbar')
+        <div class="container">
+            <div class="row justify-center">
+                <div class="col-lg-8">
+                    <div class="text-center about-us-section">
+                        <h3 class="secondary">О Сайте</h3>
+                        <img src="/img/lafeum-main-logo.png" alt="Logo lafeum">
+                        <p>Информированность о методах развития личности и совершенствования профессиональных знаний,
+                            С этой целью и создан данный ресурс, который посвящен самореализации. Здесь собраны,
+                            соответствующие
+                            тематике сайта,
+                            цитаты и афоризмы, термины научного мира и полезные ссылки на познавательно-образовательные
+                            каналы
+                            YouTube.
+                            Для удобства навигации и чтения, темы сайта разделены на четыре взаимосвязанные группы.
+                            Основным источником терминов (более 98%) является Википедия. В каждом термине есть ссылка на
+                            этот
+                            популярный ресурс,
+                            где вы можете более детально получить желаемую информацию. Комментарии и мысли специалистов
+                            взяты с
+                            популярных сайтов,
+                            на которые также указаны источники для подробного чтения.<br>
+                            Приятного и полезного чтения!</p>
+                    </div>
+                    <div>
+                        <h3 class="secondary text-center">Темы</h3>
+                        <div class="row">
+                            @foreach($categories as $category)
+                            <div class="col-lg-3 align-top mb-3">
+                                <div class="categories-main-name">
+                                    <a href="#"><b>{{ $category->name }}</b></a>
+                                </div>
+                                @foreach($category->children as $subCategory)
                                 <div class="subcategories-block">
                                     <a href="#">{{$subCategory->name}}</a><br>
                                 </div>
+                                @endforeach
+                            </div>
                             @endforeach
                         </div>
-                    @endforeach
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3 class="secondary text-center">Фото</h3>
                     </div>
-                    @foreach ($photos as $photo)
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3 class="secondary text-center">Фото</h3>
+                        </div>
+                        @foreach ($photos as $photo)
                         <div id="carouselExampleControls" class="carousel slide my-2 d-inline-block col-md-4">
                             <div class="carousel-inner main-page-carousel-inner">
                                 <div class="carousel-item active">
-                                    <img src="{{ $photo->path }}" class="d-inline-block"
-                                         alt="{{ $photo->description }}">
+                                    <img src="{{ $photo->path }}" class="d-inline-block" alt="{{ $photo->description }}">
                                 </div>
                             </div>
                         </div>
-                    @endforeach
+                        @endforeach
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    @include('layouts.right-sidebar.rightSidebar')
                 </div>
             </div>
+            @include('layouts.countItems')
         </div>
-    </div>
-    @include('layouts.right-sidebar.rightSidebar')
+    @include('layouts.footer')
 @endsection

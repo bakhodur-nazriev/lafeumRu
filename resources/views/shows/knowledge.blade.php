ae5f0c3b1f7640be5cb87cea40d010cd95ca592f
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-22 14:21:07 +0500

    Bug fix

--- resources/views/shows/knowledge.blade.php
+++ resources/views/shows/knowledge.blade.php
@@ -1,22 +1,27 @@
 @extends('layouts.default')
 
-@section('content')
+@section('left-side-bar')
     <div class="left-sidebar-block">
         <h3 class="secondary">Области знаний</h3>
         @foreach($knowledgeAreas as $knowledgeArea)
             <div><a href="#">{{ $knowledgeArea->name }}</a></div>
         @endforeach
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h3 class="secondary mb-2">{{ $currentKnowledgeArea->name }}</h3>
-                <p>{{ $currentKnowledgeArea->description }}</p>
-                @foreach($currentKnowledgeArea->terms as $term)
-                    @include('layouts.termItem')
-                @endforeach
+@endsection
+
+@section('content')
+    <h3 class="secondary mb-2">{{ $currentKnowledgeArea->name }}</h3>
+    <p>{{ $currentKnowledgeArea->description }}</p>
+    
+    @foreach($currentKnowledgeArea->terms as $term)
+        @include('layouts.termItem')
+    @endforeach
+
+    <div class="col-12">
+        <nav aria-label="Page navigation example">
+            <div class="row d-flex justify-content-center mt-3">
+                {{ $currentKnowledgeArea->terms->links() }}
             </div>
-        </div>
+        </nav>
     </div>
-    @include('layouts.right-sidebar.rightSidebar')
 @endsection

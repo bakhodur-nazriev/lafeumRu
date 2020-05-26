038880e46182d5be73ac1456be366c1dedead9fc
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-26 13:07:53 +0500

    Integrate wikipedia summary

--- public/js/summary.js
+++ public/js/summary.js
@@ -6,6 +6,17 @@ let summaryLink = null;
 
 let summaryCache = null;
 
+const summaryLinks = [
+    {
+        name: 'terms',
+        regex: new RegExp(`${window.location.origin}/[0-9]\\w+`, 'g'),
+    },
+    {
+        name: 'wikipedia',
+        regex: new RegExp('wikipedia.org/wiki/', 'g')
+    }
+];
+
 function initSummaryCache() {
     resetSummaryCache();
 
@@ -24,6 +35,8 @@ function setSummaryContent(content) {
 
 function renderSummary(summary, elementToAttach) {
     if(!summaryLink) return;
+
+    if(!summary) return;
 
     setSummaryContent(summary);
 
@@ -72,14 +85,34 @@ function showTermSummary(link, anchor) {
                 return fetchAndCacheSummary(link, anchor);
             }
         })
-        .then(r => renderSummary(r.body, anchor));
+        .then(r => renderSummary(r.extract_html, anchor));
 }
 
-function getSummaryLink(anchor) {
+function getTermSummaryLink(anchor) {
+    if(anchor.host !== window.location.host) return;
 
     let termsPostId = parseInt(anchor.pathname.replace('/', ''));
 
     return '/api/summary/' + termsPostId;
+}
+
+function getWikipediaSummaryLink(anchor) {
+    let link = anchor.href;
+
+    link = link.replace('/wiki/', '/api/rest_v1/page/summary/');
+
+    return link;
+}
+
+function getSummaryLink(anchor) {
+    let summaryLinkType = summaryLinks.find(h => anchor.href.match(h.regex));
+
+    switch(summaryLinkType.name){
+        case 'terms':
+            return getTermSummaryLink(anchor);
+        case 'wikipedia':
+            return getWikipediaSummaryLink(anchor);
+    }
 }
 
 function onMouseOverAnchor(e) {
@@ -87,8 +120,10 @@ function onMouseOverAnchor(e) {
 
     if(!anchor) return;
 
-    let currentSummaryLink = getSummaryLink(anchor); 
+    let currentSummaryLink = getSummaryLink(anchor);
     
+    if(!currentSummaryLink) return;
+
     summaryLink = currentSummaryLink;
 
     // Show modal on delay of 200Ms
@@ -108,11 +143,15 @@ function attachSummaryModals() {
 
         if(ignoreAnchor) return;
 
-        // Match only anchors with href to post (/1234)
-        if(a.pathname.match(/\/[0-9]\w+/g)){
-            a.addEventListener('mouseover', onMouseOverAnchor);
-            a.addEventListener('mouseout', hideSummary);
+        for (const supportedLink of summaryLinks) {
+            if(a.href.match(supportedLink.regex)){
+                a.addEventListener('mouseover', onMouseOverAnchor);
+                a.addEventListener('mouseout', hideSummary);
+
+                break;
+            }
         }
+        
     });
 }
 

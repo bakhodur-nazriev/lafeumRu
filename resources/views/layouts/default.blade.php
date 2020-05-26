b3e9d909ccea0b355ecd3d9334d222c5615442b7
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-26 09:52:55 +0500

    Move favourite to separate file

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -31,46 +31,7 @@
     </div>
 
     @include('layouts.footer')
-    <script>
-        function toggleFavourite(id, model, onSuccess = null, onFailure = null) {
-            $.ajax({
-                url: '/toggle-favourite',
-                method: 'PUT',
-                data: {
-                    "_token": window.Laravel.csrf_token,
-                    favouritable: model,
-                    id: id
-                }
-            }).then(onSuccess).catch(onFailure);
-        }
+    <script src="/js/favourite.js"></script>
 
-        $(document).ready(() => {
-            $('.favourite-quote-btn').click(e => {
-                let button = e.target;
-                let id = button.dataset.id;
-
-                toggleFavourite(id, "App\\Quote", (isFavourite) => {
-                    button.classList.toggle('fa-star-active', isFavourite);
-                });
-            });
-
-            $('.favourite-term-btn').click(e => {
-                let button = e.target;
-                let id = button.dataset.id;
-
-                toggleFavourite(id, "App\\Term", (isFavourite) => {
-                    button.classList.toggle('fa-star-active', isFavourite);
-                });
-            });
-
-            $('.favourite-video-btn').click(e => {
-                let button = e.target;
-                let id = button.dataset.id;
-
-                toggleFavourite(id, "App\\Video", (isFavourite) => {
-                    button.classList.toggle('fa-star-active', isFavourite);
-                });
-            });
-        });
-    </script>
+    
 @endsection

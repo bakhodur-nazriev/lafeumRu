85331378f877c6fd86f35f426fd8d44952df3df3
Ergashev Akbar <ergashev.akb@gmail.com>
2020-05-29 11:54:39 +0500

    Bug fix and refactor

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Quote;
 use App\Term;
+use App\Video;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -12,7 +14,36 @@ class PostsController extends Controller
     {
         $item = $post->postable;
 
-        return view('shows.post', compact('post', 'item'));
+        $postMetatags = [];
+
+        $postMetatags['article'] = [
+            'section' => 'Авторы',
+            'published' => $item->created_at->toAtomString(),
+            'updated' => $item->updated_at->toAtomString()
+        ];
+    
+        switch ($post->postable_type) {
+            case Quote::class:
+                $quoteBody = mb_substr(strip_tags($item->body), 0, 60);
+                $quoteAuthor = $item->author->name;
+    
+                $postMetatags['title'] = "$quoteAuthor: $quoteBody...  ЛАФЕЮМ";
+                break;
+
+            case Term::class:
+                $termBody = mb_substr(strip_tags($item->body), 0, 60);
+    
+                $postMetatags['title'] = "$termBody... - ЛАФЕЮМ";
+                $postMetatags['description'] = mb_substr(strip_tags($item->body), 0, 150) .  '...';
+                break;
+
+            case Video::class:
+
+                $postMetatags['title'] = $item->title;
+                break;
+        }
+
+        return view('shows.post', compact('post', 'item', 'postMetatags'));
     }
 
     public function termSummary($id)
--- resources/views/shows/post.blade.php
+++ resources/views/shows/post.blade.php
@@ -1,38 +1,5 @@
 @extends('layouts.default')
 
-@php
-    $postMetatags = [];
-
-    $postMetatags['article'] = [
-        'section' => 'Авторы',
-        'published' => $item->created_at->toAtomString(),
-        'updated' => $item->updated_at->toAtomString()
-    ];
-
-    switch ($post->postable_type) {
-        case 'App\\Quote':
-            
-            $quoteBody = substr(strip_tags($item->body), 0, 60);
-
-            $postMetatags['title'] = "{$item->author->name}: $quoteBody... - ЛАФЕЮМ";
-
-            break;
-        case 'App\\Term':
-
-            $termBody = substr(strip_tags($item->body), 0, 60);
-
-            $postMetatags['title'] = "$termBody... - ЛАФЕЮМ";
-            $postMetatags['description'] = substr(
-                strip_tags($item->body), 0, 150
-            ) . " ...";
-
-            break;
-        case 'App\\Video':
-            $postMetatags['title'] = $item->title;
-            break;
-    }
-@endphp
-
 @section('meta-tags')
     @include('layouts.meta-tags', $postMetatags)
 @endsection

ef54f9d90c7e5daac417580997431a6ae22d616b
Ergashev Akbar <ergashev.akb@gmail.com>
2020-06-03 12:40:20 +0500

    Remove unused code

--- app/Knowledge.php
+++ app/Knowledge.php
@@ -4,12 +4,9 @@ namespace App;
 
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
-use ChristianKuri\LaravelFavorite\Traits\Favoriteable;
 
 class Knowledge extends Model
 {
-    use Favoriteable;
-
     protected $table = "knowledge";
     protected $fillable = ["name", "description", "slug"];
 

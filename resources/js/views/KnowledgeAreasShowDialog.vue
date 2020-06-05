c61d9d58b8218bbe3205ff2f45fb08e7fc8992ae
Ergashev Akbar <ergashev.akb@gmail.com>
2020-06-05 09:29:15 +0500

    Implement show for knowledge areas

---
+++ resources/js/views/KnowledgeAreasShowDialog.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-dialog v-model="showDialog" width="600">
+        <v-card v-if="showDialog">
+            <v-card-title class="pr-2 primary white--text mb-3">
+                {{ knowledgeArea.name }}
+                <v-spacer />
+                <v-btn icon @click="editCategory">
+                    <v-icon color="white" small>mdi-pencil</v-icon>
+                </v-btn>
+                <v-btn icon @click="deleteCategory">
+                    <v-icon color="white" small>mdi-delete</v-icon>
+                </v-btn>
+            </v-card-title>
+
+            <v-card-text class="subtitle-1 pb-0">
+                {{ knowledgeArea.description || "Нет описания" }}
+            </v-card-text>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                    color="primary darken-1"
+                    text
+                    @click="closeDialog"
+                >
+                    Закрыть
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    props: {
+        knowledgeArea: Object
+    },
+    methods:{
+        closeDialog(){
+            this.$emit('close');
+        },
+        editCategory(){
+            this.$emit('edit', this.knowledgeArea);
+        },
+        deleteCategory(){
+            this.$emit('delete', this.knowledgeArea);
+        }
+    },
+    computed: {
+        showDialog: {
+            get(){
+                return this.knowledgeArea;
+            },
+            set(show){
+                if(!show){
+                    this.closeDialog();
+                }
+            }
+        }
+    }
+};
+</script>

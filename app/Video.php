773715fff241cb19b1948278713e0dbf2108e1d9
Ergashev Akbar <ergashev.akb@gmail.com>
2020-06-08 17:54:30 +0500

    Refactor and add new computed attributes

--- app/Video.php
+++ app/Video.php
@@ -10,6 +10,7 @@ class Video extends Model
     use Favoriteable;
 
     protected $fillable = ["title", "channel_id", "link", "duration"];
+    protected $appends = ["link", "thumbnail"];
 
     public function channel()
     {
@@ -25,4 +26,78 @@ class Video extends Model
     {
         return $this->morphOne(Post::class, 'postable');
     }
+
+    public function host()
+    {
+        return $this->belongsTo(VideoHost::class, 'host_type_id');
+    }
+
+    public function setLinkAttribute($value)
+    {
+        $hostData = $this->getHostData($value);
+
+        $this->attributes['host_type_id'] = $hostData['host_type_id'];
+        $this->attributes['host_id'] = $hostData['host_id'];
+    }
+
+    public function getLinkAttribute()
+    {
+        switch ($this->host_type_id) {
+            case VideoHost::YOUTUBE_HOST_TYPE_ID:
+                return "https://youtu.be/" . $this->host_id;
+
+            default:
+                return $this->host_id;
+        }
+    }
+
+    public function getThumbnailAttribute()
+    {
+        switch ($this->host_type_id) {
+            case VideoHost::YOUTUBE_HOST_TYPE_ID:
+                return "https://i.ytimg.com/vi/{$this->host_id}/mqdefault.jpg";
+
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Helpers
+     * 
+     */
+
+    public function getHostData($link)
+    {
+        $hostData = [
+            'host_type_id' => VideoHost::UNKNOWN_HOST_TYPE_ID,
+            'host_id' => $link
+        ];
+
+        $youtubeId = $this->getYoutubeId($link);
+
+        if($youtubeId){
+            $hostData['host_type_id'] = VideoHost::YOUTUBE_HOST_TYPE_ID;
+            $hostData['host_id'] = $youtubeId;
+        }
+
+        return $hostData;
+    }
+
+    private function getYoutubeId($link)
+    {
+        $youtubeIdRegEx = '/(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/';
+
+        $pregMatchOutput = [];
+
+        $matched = preg_match($youtubeIdRegEx, $link, $pregMatchOutput);
+
+        if($matched && count($pregMatchOutput) === 2){
+            return $pregMatchOutput[1];
+        }
+
+        return null;
+    }
+
+
 }

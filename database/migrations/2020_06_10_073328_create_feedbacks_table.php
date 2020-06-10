ebf1c4c7fdf5e3a7cedba3feefc7ce64d8db5968
Ergashev Akbar <ergashev.akb@gmail.com>
2020-06-10 12:47:55 +0500

    Add table to store feedbaacks

---
+++ database/migrations/2020_06_10_073328_create_feedbacks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedbacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedbacks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('user_name');
+            $table->string('user_email');
+            $table->string('topic');
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedbacks');
+    }
+}

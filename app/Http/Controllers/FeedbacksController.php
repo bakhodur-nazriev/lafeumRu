35f37fa0cc88fdb32ccbea768fdce09467c7a546
Ergashev Akbar <ergashev.akb@gmail.com>
2020-06-12 08:38:19 +0500

    Refactor

--- app/Http/Controllers/FeedbacksController.php
+++ app/Http/Controllers/FeedbacksController.php
@@ -7,8 +7,30 @@ use Illuminate\Http\Request;
 
 class FeedbacksController extends Controller
 {
-    public function get()
+    public function create()
     {
-        return Feedback::all();
+        return view('/contacts');
+    }
+
+    public function get(Request $request)
+    {
+        return Feedback::latest()->paginate($request->perPage ?: 15);
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'user_name' => 'required',
+            'user_email' => 'required|email',
+            'topic' => 'required',
+            'message' => 'required'
+        ]);
+
+        Feedback::create($data);
+
+        // Mail::to(env('CONTACT_EMAIL', 'test@test.com'))
+        //     ->send(new ContactMail($data));
+
+        return redirect('/contacts');
     }
 }

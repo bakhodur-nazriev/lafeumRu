977f88979110030bd33dcbe63239f855ff85cfea
Ergashev Akbar <ergashev.akb@gmail.com>
2020-06-16 11:19:02 +0500

    Improve usability of videos

--- resources/views/layouts/videoItem.blade.php
+++ resources/views/layouts/videoItem.blade.php
@@ -22,23 +22,21 @@
                 </div>
             @else
                 <div class="col-md-4 col-12 py-0">
-                    <div
-                        class="modal fade"
-                        id="video-{{$video->id}}-modal"
-                        tabindex="-1"
+                    <div 
+                        class="modal fade" 
+                        id="video-{{$video->post->id}}-modal" 
+                        tabindex="-1" 
                         role="dialog"
-                        data-backdrop="static"
-                        data-keyboard="false"
-                        aria-labelledby="video-{{$video->id}}-modal-title"
+                        aria-labelledby="video-{{$video->post->id}}-modal-title"
                         style="display: none;"
                         aria-hidden="true"
                     >
                         <div class="modal-dialog modal-dialog-centered modal-lg">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5
-                                        class="modal-title"
-                                        id="video-{{$video->id}}-modal-title"
+                                    <h5 
+                                        class="modal-title" 
+                                        id="video-{{$video->post->id}}-modal-title"
                                     >
                                         {{$video->title}}
                                     </h5>
@@ -64,10 +62,9 @@
                             </div>
                         </div>
                     </div>
-                    <div
-                        class="text-right position-relative"
-                        data-toggle="modal"
-                        data-target="#video-{{$video->id}}-modal"
+                    <div 
+                        class="text-right position-relative cursor-pointer mb-2"
+                        onclick="showVideoModal('#video-{{$video->post->id}}-modal', {{$video->post->id}})"
                     >
                         <img class="img-thumbnail w-100" src="{{$video->thumbnail}}"/>
                         <span class="video-duration-block">{{$video->duration}}:00</span>
@@ -76,9 +73,8 @@
             @endif
             <div class="{{$fullVariant ? 'col-12': 'col-md-8 col-12 pl-md-0 py-0'}}">
                 <div class="d-flex flex-row justify-content-between">
-                    <h5 class="card-title d-inline-block" data-toggle="modal"
-                        data-target="#video-{{$video->id}}-modal">{{$video->title}}</h5>
-                    <div class="d-flex flex-row" style="height: fit-content">
+                    <h5 class="card-title d-inline-block cursor-pointer" onclick="showVideoModal('#video-{{$video->post->id}}-modal', {{$video->post->id}})">{{$video->title}}</h5>
+                    <div class="ml-4 d-flex flex-row" style="height: fit-content">
                         <a class="secondary" href="/{{$video->post->id}}">
                             #{{$video->post->id}}
                         </a>

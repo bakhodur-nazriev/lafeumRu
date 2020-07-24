88978f8b153ed3f7183b004c80e9c21f9d013cbf
Ergashev Akbar <ergashev.akb@gmail.com>
2020-07-24 18:06:02 +0500

    Made daily posts clickable

--- resources/views/layouts/right-sidebar/quote.blade.php
+++ resources/views/layouts/right-sidebar/quote.blade.php
@@ -1,9 +1,11 @@
 <div class="right-sidebar-block my-2">
-    <div class="title-of-right-sidebar-posts">
-        <h5 class="pl-3 py-2 mb-0">Цитата дня</h5>
-    </div>
+    <a href="/{{$dailyPosts->quote->post->id}}">
+        <div class="title-of-right-sidebar-posts">
+            <h5 class="pl-3 py-2 mb-0">Цитата дня</h5>
+        </div>
+    </a>
     <div class="p-3">
-        <p class="mb-1 five-line-truncate">{!! $dailyPosts['quote']->body !!}</p>
-        <p class="text-right font-italic m-0">{{ $dailyPosts['quote']['author']->name }}</p>
+        <p class="mb-1 five-line-truncate">{!! $dailyPosts->quote->body !!}</p>
+        <p class="text-right font-italic m-0">{{ $dailyPosts->quote->author->name }}</p>
     </div>
 </div>

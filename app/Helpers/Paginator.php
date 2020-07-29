55f2b2f65bd2a70a0b82ec92d2b871af2a26ba89
Ergashev Akbar <ergashev.akb@gmail.com>
2020-07-29 18:20:38 +0500

    Add custom collection paginator

---
+++ app/Helpers/Paginator.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Collection;
+
+class Paginator implements Arrayable {
+
+    const DEFAULT_PAGE = 1;
+    const DEFAULT_ITEMS_PER_PAGE = 30;
+
+    private $items;
+
+    private $perPage;
+
+    private $currentPage;
+
+    public function __construct(Collection $items, $perPage, $currentPage) {
+        $this->items = $items;
+        $this->perPage = $perPage ?: self::DEFAULT_ITEMS_PER_PAGE;
+        $this->currentPage = $currentPage ?: self::DEFAULT_PAGE;
+
+        if($perPage == "all"){
+            $this->perPage = $items->count();
+        }
+    }
+
+    public function toArray()
+    {
+        return [
+            "current_page" => $this->currentPage,
+            "last_page" => $this->getTotalPages(),
+            "per_page" => $this->perPage,
+            "total" => $this->items->count(),
+            "data" => $this->items->forPage($this->currentPage, $this->perPage)->values()
+        ];
+    }
+
+    public function getData()
+    {
+        return $this->items;
+    }
+
+    public function setData($data)
+    {
+        $this->items = $data;
+    }
+
+    private function getTotalPages()
+    {
+        $floatOfTotalPages = $this->items->count() / $this->perPage;
+
+        $totalPages = intval($floatOfTotalPages);
+
+        if($floatOfTotalPages - $totalPages > 0){
+            $totalPages++;
+        }        
+
+        return $totalPages;
+    }
+}

a11d6fdf3ba3135ec5b75aba816cc7e5f55d24ea
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2020-10-07 17:59:02 +0500

    Fix WysiwygEditor.vue

--- resources/js/components/WysiwygEditor.vue
+++ resources/js/components/WysiwygEditor.vue
@@ -2,6 +2,7 @@
     <froala
         :label="label"
         :value="value"
+        :config="config"
         @input="textChanged"
     />
 </template>
@@ -10,6 +11,7 @@
 
 //Import Froala Editor
 import 'froala-editor/js/plugins.pkgd.min.js';
+import 'froala-editor/js/languages/ru.js';
 
 //Import third party plugins
 import 'froala-editor/js/third_party/embedly.min';
@@ -19,6 +21,7 @@ import 'froala-editor/js/third_party/image_tui.min';
 
 // Import Froala Editor css files.
 import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/css/themes/gray.min.css';
 
 // Import and use Vue Froala lib.
 import VueFroala from 'vue-froala-wysiwyg';
@@ -27,6 +30,33 @@ export default {
     props: {
         value: String,
         label: String
+    },
+    data() {
+        return {
+            config: {
+                theme: 'gray',
+                zIndex: 2001,
+                language: 'ru',
+                placeholderText: this.label,
+                toolbarButtons: {
+                    moreText: {
+                        buttons: ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
+                    },
+                    moreParagraph: {
+                        buttons: ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote']
+                    },
+                    moreRich: {
+                        buttons: ['insertLink', 'html']
+                    },
+                    moreMisc: {
+                        align: 'right',
+                        buttonsVisible: 2,
+                        buttons: ['undo', 'redo', 'fullscreen']
+                    }
+
+                },
+            }
+        }
     },
     beforeMount() {
         window.Vue.use(VueFroala);

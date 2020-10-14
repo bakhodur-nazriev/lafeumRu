5e0c71f725675bb36baf3299a5e9bba5731769a1
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2020-10-14 17:58:41 +0500

    Remove atom icons from terms

--- resources/js/views/CategoryShowDialog.vue
+++ resources/js/views/CategoryShowDialog.vue
@@ -3,12 +3,21 @@
         <v-card v-if="showDialog">
             <v-card-title class="pr-2 primary white--text mb-3">
                 {{ category.name }}
+                <v-spacer/>
+                <v-btn icon @click="showCategoryPage">
+                    <v-icon color="white" small>mdi-eye</v-icon>
+                </v-btn>
+                <v-btn icon @click="editCategory">
+                    <v-icon color="white" small>mdi-pencil</v-icon>
+                </v-btn>
+                <v-btn icon @click="deleteCategory">
+                    <v-icon color="white" small>mdi-delete</v-icon>
+                </v-btn>
             </v-card-title>
-
             <v-card-text v-html="category.description"/>
 
             <v-card-actions>
-                <v-spacer></v-spacer>
+                <v-spacer/>
                 <v-btn text @click="closeDialog" color="primary darken-1">Закрыть</v-btn>
             </v-card-actions>
         </v-card>

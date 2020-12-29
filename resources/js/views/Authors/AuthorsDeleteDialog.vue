f29fb2398755fe2668b890d8501b5b2fb40d58e0
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2020-12-29 17:44:45 +0500

    Fix authors and channels vue title

--- resources/js/views/Authors/AuthorsDeleteDialog.vue
+++ resources/js/views/Authors/AuthorsDeleteDialog.vue
@@ -1,24 +1,12 @@
 <template>
-    <v-dialog v-if="authorToDelete" v-model="authorToDelete" width="500">
-        <v-card class="pa-2">
-            <p class="pt-1 regular headline text-center">
-                Вы действительно хотите удалить <br/>автора ?
-            </p>
+    <v-dialog v-if="authorToDelete" v-model="authorToDelete" width="430">
+        <v-card class="pa-1">
+            <v-card-title class="font-weight-regular headline text-center pa-2">
+                Вы действительно хотите удалить автора ?
+            </v-card-title>
             <v-card-actions class="justify-center">
-                <v-btn
-                    dark
-                    color="green darken-1"
-                    @click="$emit('input', null)"
-                >
-                    Нет
-                </v-btn>
-                <v-btn
-                    dark
-                    color="red darken-1"
-                    @click="deleteAuthor()"
-                >
-                    Да
-                </v-btn>
+                <v-btn dark color="green darken-1" @click="$emit('input', null)">Нет</v-btn>
+                <v-btn dark color="red darken-1" @click="deleteAuthor()">Да</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
--- resources/js/views/Channels/ChannelsDeleteDialog.vue
+++ resources/js/views/Channels/ChannelsDeleteDialog.vue
@@ -1,20 +1,12 @@
 <template>
-    <v-dialog v-if="channelToDelete" v-model="channelToDelete" width="500">
-        <v-card class="pa-2">
-            <v-card-title class="pt-1 regular headline text-center">
-                Вы действительно хотите удалить этот канал ?
+    <v-dialog v-if="channelToDelete" v-model="channelToDelete" width="430">
+        <v-card class="pa-1">
+            <v-card-title class="font-weight-regular headline text-center pa-2">
+                Вы действительно хотите удалить канал ?
             </v-card-title>
             <v-card-actions class="justify-center">
-                <v-btn
-                    color="green darken-1"
-                    dark
-                    @click="$emit('input', null)"
-                >
-                    Нет
-                </v-btn>
-                <v-btn color="red darken-1" dark @click="deleteChannel()"
-                    >Да</v-btn
-                >
+                <v-btn dark color="green darken-1" @click="$emit('input', null)">Нет</v-btn>
+                <v-btn color="red darken-1" dark @click="deleteChannel()">Да</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>

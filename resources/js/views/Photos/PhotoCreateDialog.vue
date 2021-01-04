b57416ea3654f4085ba5f18aa5bd43ab878c21e7
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2021-01-04 16:07:09 +0500

    Moved vue components

---
+++ resources/js/views/Photos/PhotoCreateDialog.vue
@@ -0,0 +1,106 @@
+<template>
+    <v-dialog :value="value" width="750" @input="$emit('input', false)">
+        <v-card>
+            <v-form @submit="addPhoto" ref="createForm">
+                <v-card-title class="primary white--text pa-4">
+                    Добавить фото
+                </v-card-title>
+                <v-container>
+                    <v-row justify="center">
+                        <v-col cols="12 py-0">
+                            <v-file-input
+                                outlined
+                                prepend-icon=""
+                                label="Выберите фото"
+                                :rules="[rules.required]"
+                                v-model="newPhoto.image"
+                                prepend-inner-icon="mdi-camera"
+                            />
+                        </v-col>
+                        <v-col cols="12 py-0">
+                            <date-picker
+                                with-time
+                                label="Дата публикации"
+                                v-model="newPhoto.publish_at"
+                            />
+                        </v-col>
+                        <v-col cols="12 py-0">
+                            <v-textarea
+                                outlined
+                                :rules="[rules.required]"
+                                label="Добавить описание"
+                                v-model="newPhoto.description"
+                            />
+                        </v-col>
+                    </v-row>
+                </v-container>
+                <v-card-actions class="p-3">
+                    <v-spacer/>
+                    <v-btn dark color="primary" type="submit">Сохранить</v-btn>
+                    <v-btn dark color="primary" type="button" @click="$emit('input', false)">Отмена</v-btn>
+                </v-card-actions>
+            </v-form>
+
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    import rules from "../../validation-rules";
+    import DatePicker from "../../components/DatePicker";
+
+    export default {
+        props: {value: Boolean},
+        components: {"date-picker": DatePicker},
+        data() {
+            return {
+                rules,
+                newPhoto: null,
+            }
+        },
+        beforeMount() {
+            this.newPhoto = this.getDefaultPhoto();
+        },
+        methods: {
+            getDefaultPhoto() {
+                return {
+                    image: null,
+                    description: "",
+                    publish_at: ""
+                };
+            },
+            resetNewPhoto() {
+                this.newPhoto = this.getDefaultPhoto();
+            },
+            addPhoto(e) {
+                e.preventDefault();
+
+                const validForm = this.$refs.createForm.validate();
+
+
+                if (!validForm) return;
+
+                const formData = new FormData();
+
+                formData.append("image", this.newPhoto.image);
+                formData.append("publish_at", this.newPhoto.publish_at);
+                formData.append("description", this.newPhoto.description);
+
+                axios
+                    .post("/api/photos", formData, {
+                        headers: {
+                            "Content-Type": "multipart/form-data"
+                        }
+                    })
+                    .then(res => {
+                        this.resetNewPhoto();
+                        this.$emit('created', res.data);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            },
+        }
+    }
+</script>
+
---
+++ resources/js/views/Terms/TermsPage.vue
@@ -0,0 +1,156 @@
+<template>
+    <v-main class="pa-0">
+        <index-page-layout
+            ref="indexPage"
+            search-field="body"
+            index-url="/api/terms"
+            :categories="categories"
+            :table-headers="this.headers"
+            add-label="Добавить термин"
+            @show-item="showTerm"
+            @add-item="addTerm = true"
+            @update-item="termToUpdate = $event"
+            @delete-item="termToDelete = $event"
+        >
+            <template v-slot:item.body="{ item }">
+                <div
+                    v-html="item.body"
+                    class="my-3 three-line-truncate"
+                />
+            </template>
+            <template v-slot:item.knowledge="{ item }">
+                <div
+                    v-for="(know, i) in item.knowledge"
+                    :key="i"
+                >
+                    {{ know.name }},
+                </div>
+            </template>
+            <template v-slot:item.categories="{ item }">
+                <div
+                    v-for="(category, i) in item.categories"
+                    :key="i"
+                >
+                    {{ category.name }},
+                </div>
+            </template>
+        </index-page-layout>
+
+        <terms-create-dialog
+            v-model="addTerm"
+            :knowledge-areas="knowledgeAreas"
+            :categories="categories"
+            :term-types="termTypes"
+            @created="termCreated"
+        />
+
+        <terms-edit-dialog
+            v-model="termToUpdate"
+            :knowledge-areas="knowledgeAreas"
+            :term-types="termTypes"
+            :categories="categories"
+            @updated="termUpdated"
+        />
+
+        <terms-delete-dialog
+            v-model="termToDelete"
+            @deleted="termDeleted"
+        />
+    </v-main>
+</template>
+<script>
+import IndexPageLayout from "../../components/IndexPageLayout";
+import TermsCreateDialog from "./TermsCreateDialog";
+import TermsEditDialog from "./TermsEditDialog";
+import TermsDeleteDialog from "./TermsDeleteDialog";
+
+export default {
+    components: {
+        IndexPageLayout,
+        TermsCreateDialog,
+        TermsEditDialog,
+        TermsDeleteDialog,
+    },
+    data() {
+        return {
+            addTerm: false,
+            termTypes: [],
+            categories: [],
+            knowledgeAreas: [],
+            termToUpdate: null,
+            termToDelete: null,
+            headers: [
+                {
+                    text: "Словарь",
+                    value: "name"
+                },
+                {
+                    text: "Термины",
+                    value: "body",
+                    class: "ma-3"
+                },
+                {
+                    text: "Тип",
+                    value: "term_type.name",
+                    align: "center"
+                },
+                {
+                    text: "Область знаний",
+                    value: "knowledge",
+                    align: "center"
+                },
+                {
+                    text: "Рубрики",
+                    value: "categories",
+                    align: "center"
+                },
+                {
+                    text: "Опубликовано",
+                    value: "publish_at",
+                    width: 160
+                }
+            ]
+        };
+    },
+    mounted() {
+        this.loadKnowledgeAreas();
+        this.loadTermCategories();
+        this.loadTermTypes();
+    },
+    methods: {
+        loadTermTypes() {
+            axios
+                .get("/api/term-types")
+                .then(res => (this.termTypes = res.data))
+                .catch(e => console.log(e));
+        },
+        loadKnowledgeAreas() {
+            axios
+                .get("/api/knowledge-areas")
+                .then(res => (this.knowledgeAreas = res.data))
+                .catch(e => console.log(e));
+        },
+        loadTermCategories() {
+            axios
+                .get("/api/categories?type=" + TERM_TYPE)
+                .then(res => (this.categories = res.data))
+                .catch(e => console.log(e));
+        },
+        termCreated(newTerm) {
+            this.addTerm = false;
+            this.$refs.indexPage.loadItems();
+        },
+        termUpdated(newTerm) {
+            this.termToUpdate = null;
+            this.$refs.indexPage.loadItems();
+        },
+        termDeleted(newTerm) {
+            this.termToDelete = null;
+            this.$refs.indexPage.loadItems();
+        },
+        showTerm(term) {
+            window.open('/' + term.post.id, '_black');
+        }
+    }
+};
+</script>

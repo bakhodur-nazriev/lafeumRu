ed3b2fb2e1f2cc80437799307d828c17cb1bfea4
Dzhabarov Anvar <darksider_5@mail.ru>
2021-01-11 16:01:35 +0500

    Fixed two column on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,72 +21,75 @@
 <div>
     <h3 class="secondary text-center">Темы</h3>
     <div class="row main-themes">
-        @for($i = 0; $i < 9; $i=$i+3) <div class="col-lg-4 offset-lg-0 col-md-3 offset-md-1 align-top">
-            <div class="dropdown">
-                <button id="theme-{{ $categories[$i]->slug }}" type="button" data-toggle="dropdown"
-                    class="btn btn-link categories-color dropdown-toggle py-0">
-                    <span class="lead font-weight-bold">{{ $categories[$i]->name }}</span>
-                </button>
+            @for( $i = 0; $i <= 3; $i = $i+3 )
+            <div class="col-lg-1 offset-lg-3 col-md-3 offset-md-1 align-top">
+                <div class="dropdown">
+                    <button id="theme-{{ $categories[$i]->slug }}" type="button" data-toggle="dropdown"
+                        class="btn btn-link categories-color dropdown-toggle py-0">
+                        <span class="lead font-weight-bold">{{ $categories[$i]->name }}</span>
+                    </button>
 
-                <div class="dropdown-menu py-0">
-                    @foreach($categories[$i]->post_links as $postsLink)
-                    <a class="dropdown-item home-themes-dropdown"
-                        href="{{ $postsLink['href'] }}">{{$postsLink['label']}}</a>
-                    <div class="dropdown-divider m-0"></div>
-                    @endforeach
-                </div>
+                    <div class="dropdown-menu py-0">
+                        @foreach($categories[$i]->post_links as $postsLink)
+                        <a class="dropdown-item home-themes-dropdown"
+                            href="{{ $postsLink['href'] }}">{{$postsLink['label']}}</a>
+                        <div class="dropdown-divider m-0"></div>
+                        @endforeach
+                    </div>
 
-            </div>
-            @foreach($categories[$i]->children as $subCategory)
-            <div class="dropdown">
-                <button id="theme-{{ $subCategory->slug }}" type="button" data-toggle="dropdown"
-                    class="btn btn-link categories-color dropdown-toggle py-0">
-                    {{ $subCategory->name }}
-                </button>
-                <div class="dropdown-menu py-0">
-                    @foreach($subCategory->post_links as $postsLink)
-                    <a class="dropdown-item home-themes-dropdown"
-                        href="{{ $postsLink['href'] }}">{{$postsLink['label']}}</a>
-                    <div class="dropdown-divider m-0"></div>
-                    @endforeach
                 </div>
-            </div>
-            @endforeach
+                @foreach($categories[$i]->children as $subCategory)
+                <div class="dropdown">
+                    <button id="theme-{{ $subCategory->slug }}" type="button" data-toggle="dropdown"
+                        class="btn btn-link categories-color dropdown-toggle py-0">
+                        {{ $subCategory->name }}
+                    </button>
+                    <div class="dropdown-menu py-0">
+                        @foreach($subCategory->post_links as $postsLink)
+                        <a class="dropdown-item home-themes-dropdown"
+                            href="{{ $postsLink['href'] }}">{{$postsLink['label']}}</a>
+                        <div class="dropdown-divider m-0"></div>
+                        @endforeach
+                    </div>
+                </div>
+                @endforeach
+                
+                @if($i === 3)
+                    @while ( $i <= 6)
+                        <div class="dropdown" style="margin-top: 20px">
+                            <button id="theme-{{ $categories[$i+3]->slug }}" type="button" data-toggle="dropdown"
+                                class="btn btn-link categories-color dropdown-toggle py-0">
+                                <span class="lead font-weight-bold">{{ $categories[$i+3]->name }}</span>
+                            </button>
 
-            @if($i === 6)
-            <div class="dropdown" style="margin-top: 20px">
-                <button id="theme-{{ $categories[$i+3]->slug }}" type="button" data-toggle="dropdown"
-                    class="btn btn-link categories-color dropdown-toggle py-0">
-                    <span class="lead font-weight-bold">{{ $categories[$i+3]->name }}</span>
-                </button>
-
-                <div class="dropdown-menu py-0">
-                    @foreach($categories[$i+3]->post_links as $postsLink)
-                    <a class="dropdown-item home-themes-dropdown"
-                        href="{{ $postsLink['href'] }}">{{$postsLink['label']}}</a>
-                    <div class="dropdown-divider m-0"></div>
-                    @endforeach
-                </div>
+                            <div class="dropdown-menu py-0">
+                                @foreach($categories[$i+3]->post_links as $postsLink)
+                                <a class="dropdown-item home-themes-dropdown"
+                                    href="{{ $postsLink['href'] }}">{{$postsLink['label']}}</a>
+                                <div class="dropdown-divider m-0"></div>
+                                @endforeach
+                            </div>
+                        </div>
+                        @foreach($categories[$i+3]->children as $subCategory)
+                        <div class="dropdown">
+                            <button id="theme-{{ $subCategory->slug }}" type="button" data-toggle="dropdown"
+                                class="btn btn-link categories-color dropdown-toggle py-0">
+                                {{ $subCategory->name }}
+                            </button>
+                            <div class="dropdown-menu py-0">
+                                @foreach($subCategory->post_links as $postsLink)
+                                <a class="dropdown-item home-themes-dropdown"
+                                    href="{{ $postsLink['href'] }}">{{$postsLink['label']}}</a>
+                                <div class="dropdown-divider m-0"></div>
+                                @endforeach
+                            </div>
+                        </div>
+                        @endforeach
+                        @php $i = $i+3; @endphp
+                    @endwhile
+                @endif
             </div>
-            @foreach($categories[$i+3]->children as $subCategory)
-            <div class="dropdown">
-                <button id="theme-{{ $subCategory->slug }}" type="button" data-toggle="dropdown"
-                    class="btn btn-link categories-color dropdown-toggle py-0">
-                    {{ $subCategory->name }}
-                </button>
-                <div class="dropdown-menu py-0">
-                    @foreach($subCategory->post_links as $postsLink)
-                    <a class="dropdown-item home-themes-dropdown"
-                        href="{{ $postsLink['href'] }}">{{$postsLink['label']}}</a>
-                    <div class="dropdown-divider m-0"></div>
-                    @endforeach
-                </div>
-            </div>
-            @endforeach
-            @php $i = $i+3; @endphp
-            @endif
+        @endfor
     </div>
-    @endfor
-</div>
 </div>
 @endsection

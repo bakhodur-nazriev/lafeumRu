40e05c9d53dcfa58ac3f923b007af5fef2b48486
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2021-02-24 18:58:57 +0500

    Create pages vue for frontend

---
+++ resources/js/components/front/Default.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Defaule Page</h1>
+</template>
+
+<script>
+
+export default {
+    name: "Default",
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/front/Quotes.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "Quotes"
+}
+</script>
+
+<style scoped>
+
+</style>

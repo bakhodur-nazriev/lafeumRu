8af32987db59aa05df7bd9ff11b6ce81abd2f821
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2021-02-24 19:39:00 +0500

    Create pages front with navbar and footer vue

---
+++ resources/js/components/frontApp.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app>
+        <navbar/>
+    </v-app>
+</template>
+
+<script>
+import navbar from "./layouts/FrontNavbar";
+
+export default {
+    components: {navbar},
+    name: "frontApp"
+}
+
+</script>

f7fa4e1c5f70eb6da95d52c81bdcb05293bee2d7
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2021-03-11 19:15:08 +0500

    Craete auhtor and quotes vue for front

---
+++ resources/js/views/front/Quotes.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-col cols="5">
+        <h5 class="text-uppercase font-weight-regular py-4 mx-4">Цитаты</h5>
+        <quote-item :all-quoets="allQuotes"></quote-item>
+    </v-col>
+</template>
+
+<script>
+import QuoteItem from "./layouts/QuoteItem";
+
+export default {
+    components: {QuoteItem},
+    props: ["quotes"],
+    data() {
+        return {
+            allQuotes: this.quotes
+        };
+    }
+};
+</script>
+
+<style scoped>
+.categories-block a:hover {
+    color: #04718C !important;
+}
+</style>

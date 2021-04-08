05b07545214f88ebc2fad8b08ac63742d3c3b5d6
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2021-04-08 22:31:26 +0500

    Fix home categories

--- resources/js/components/layouts/LeftSideBar.vue
+++ resources/js/components/layouts/LeftSideBar.vue
@@ -1,25 +1,43 @@
 <template>
     <v-col cols="2">
         <h5 class="text-uppercase text-secondary font-weight-normal py-4">темы</h5>
-        <v-sheet class="pa-2" rounded="lg" height="300" width="100%">
-            <v-expansion-panels flat multiple>
-                <!--                <v-expansion-panel-->
-                <!--                    v-for="(category, i) in allCategories"-->
-                <!--                    :key="i"-->
-                <!--                >-->
-                <!--                    <v-expansion-panel-header class="font-weight-medium">-->
-                <!--                        <a href="">{{ category.label }}</a>-->
-                <!--                    </v-expansion-panel-header>-->
-                <!--                    <v-expansion-panel-content>-->
-                <!--                        <div-->
-                <!--                            v-for="(subCategory, i) in category.children"-->
-                <!--                            :key="i"-->
-                <!--                            class="py-1"-->
-                <!--                        >-->
-                <!--                            <a href="" class="sub-category-links">{{ subCategory.name }}</a>-->
-                <!--                        </div>-->
-                <!--                    </v-expansion-panel-content>-->
-                <!--                </v-expansion-panel>-->
+        <v-sheet class="pa-2" rounded="lg" height="96%" width="100%">
+            <v-expansion-panels flat>
+                <v-expansion-panel>
+                    <v-expansion-panel-header class="font-weight-bold">
+                        {{ allCategories[0].name }}
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        <div
+                            v-for="(postLink ,i) in allCategories[0].post_links"
+                            :key="i"
+                            class="font-weight-normal"
+                        >
+                            <a :href="postLink.href">{{ postLink.label }}</a>
+                        </div>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
+            <v-expansion-panels flat>
+                <v-expansion-panel
+                    class="deep-expansion-panel"
+                    v-for="(subCategory ,i) in allCategories[0].children"
+                    :key="i"
+                >
+                    <v-expansion-panel-header expand-icon="">
+                        {{ subCategory.name }}
+                    </v-expansion-panel-header>
+                    <v-expansion-panel-content
+                        class="deep-expansion-panel-content pa-0"
+                    >
+                        <div
+                            v-for="(subPostLink ,i) in subCategory.post_links"
+                            :key="i"
+                        >
+                            <a :href="subPostLink.href">{{ subPostLink.label }}</a>
+                        </div>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
             </v-expansion-panels>
         </v-sheet>
     </v-col>
@@ -28,12 +46,14 @@
 <script>
 export default {
     name: "LeftSideBar",
-    // props: ["categories"],
+    props: ["categories"],
     data() {
         return {
-            // allCategories: this.categories,
-            // panel: [0, 1, 2]
+            allCategories: this.categories,
         }
+    },
+    created() {
+        console.log(this.categories);
     }
 }
 </script>
@@ -51,4 +71,15 @@ button:focus {
 .sub-category-links:hover {
     color: #04718c;
 }
+
+.v-expansion-panel > .deep-expansion-panel-header {
+    padding: 0;
+    min-height: 30px;
+}
+
+.v-expansion-panel-content__wrap {
+    padding: 0 !important;
+}
+
+
 </style>

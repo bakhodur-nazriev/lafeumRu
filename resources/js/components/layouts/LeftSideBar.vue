9af133938046a20f61bb78ae627caf26e143bfdc
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2021-04-12 19:25:33 +0500

    Fix LeftSidebar and RightSideBar

--- resources/js/components/layouts/LeftSideBar.vue
+++ resources/js/components/layouts/LeftSideBar.vue
@@ -3,23 +3,22 @@
         <h5 class="text-uppercase text-secondary font-weight-normal py-4">темы</h5>
         <v-sheet class="pa-2" rounded="lg" width="100%">
             <!-- Category -->
-            <div v-for="(icon ,i) in categoryIcons" :key="i">
 
-                <v-list-item
-                    v-for="(category, i) in allCategories"
-                    :key="i"
-                >
-                    <v-list-item-content two-line>
-                        <v-list-item-title class="mb-3">
-                            <a class="font-weight-bold" :href="category.slug">{{ category.name }}</a>
-                        </v-list-item-title>
-                        <v-list-item-subtitle class="my-1" v-for="(subCategory ,i) in category.children" :key="i">
-                            <a :href="subCategory.slug">{{ subCategory.name }}</a>
-                        </v-list-item-subtitle>
-                    </v-list-item-content>
-                </v-list-item>
+            <v-list-item
+                v-for="(category, i) in allCategories"
+                :key="i"
+            >
+                <v-list-item-content two-line>
+                    <v-list-item-title class="mb-3">
+                        <a class="font-weight-bold subtitle-1 category-links" :href="category.slug">{{ category.name }}</a>
+                    </v-list-item-title>
+                    
+                    <v-list-item-subtitle v-for="(subCategory ,i) in category.children" :key="i">
+                        <a :href="subCategory.slug" class="subtitle-1 category-links">{{ subCategory.name }}</a>
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
 
-            </div>
         </v-sheet>
     </v-col>
 </template>
@@ -45,9 +44,6 @@ export default {
                 console.log(icon.icon)
             }
         }
-    },
-    created() {
-        console.log(this.categories)
     }
 }
 </script>
@@ -57,34 +53,13 @@ button:focus {
     outline: none;
 }
 
-.sub-category-links {
-    color: #494949;
+.category-links{
+    color: #676767;
+
+}
+
+.category-links:hover{
     text-decoration: none;
-}
-
-.sub-category-links:hover {
     color: #04718c;
-}
-
-.deep-expansion-panel {
-    margin: 0;
-}
-
-.v-expansion-panel--active >
-.v-expansion-panel-header {
-    min-height: 50px;
-}
-
-.v-expansion-panel-content__wrap {
-    padding: 0 24px !important;
-}
-
-.deep-sub-expansion-panel-header {
-    padding: 0 24px;
-    min-height: 30px;
-}
-
-.deep-expansion-panel-header {
-    min-height: 30px;
 }
 </style>
--- resources/js/components/layouts/RightSideBar.vue
+++ resources/js/components/layouts/RightSideBar.vue
@@ -4,9 +4,13 @@
         <v-card class="rounded-lg" flat>
             <!-- Quote Section -->
             <div class="pa-5 pb-0">
-                <v-card-title class="subtitle-2 font-weight-medium pa-0">Цитата дня <v-spacer></v-spacer><v-icon>mdi-dots-horizontal</v-icon></v-card-title>
+                <v-card-title class="subtitle-2 font-weight-medium pa-0">Цитата дня
+                    <v-spacer></v-spacer>
+                    <v-icon>mdi-dots-horizontal</v-icon>
+                </v-card-title>
                 <v-divider></v-divider>
-                <v-card-title class="subtitle-2 font-weight-medium pa-0 mb-1">{{ quote.author.name }}<br/> Категория: Цитата дня
+                <v-card-title class="subtitle-2 font-weight-medium pa-0 mb-1">{{ quote.author.name }}<br/> Категория:
+                    Цитата дня
                 </v-card-title>
                 <v-card-text class="truncate-to-five-line" v-html="quote.body"></v-card-text>
                 <v-card-actions class="justify-content-end">
@@ -38,20 +42,45 @@
             </div>
 
             <!-- Photo Section -->
-            <div class="pa-5">
-                <v-card-title class="subtitle-2 font-weight-medium pa-0">Фото дня</v-card-title>
-                <v-divider></v-divider>
-                <v-card-text class="pa-0">
+            <v-dialog v-model="dialog" width="1000">
+                <template v-slot:activator="{ on, attrs }">
+                    <div class="pa-5">
+                        <v-card-title class="subtitle-2 font-weight-medium pa-0">Фотография дня</v-card-title>
+                        <v-divider></v-divider>
+
+                        <v-card-text
+                            class="pa-0"
+                            v-on="on"
+                            v-bind="attrs"
+                        >
+                            <v-img
+                                :src="photo.path"
+                                :alt="photo.description"
+                                max-height="150"
+                            ></v-img>
+                        </v-card-text>
+                        <v-card-actions class="justify-content-end">
+                            <a href="/photos" class="font-italic">еще.</a>
+                        </v-card-actions>
+                    </div>
+                </template>
+                <v-card>
+                    <v-card-title class="headline grey lighten-2">
+                        <v-spacer></v-spacer>
+                        <v-btn
+                            icon
+                            @click="dialog = false"
+                        >
+                            <v-icon>mdi-close</v-icon>
+                        </v-btn>
+                    </v-card-title>
+
                     <v-img
                         :src="photo.path"
                         :alt="photo.description"
-                        max-height="150"
                     ></v-img>
-                </v-card-text>
-                <v-card-actions class="justify-content-end">
-                    <a href="/photos" class="font-italic">еще.</a>
-                </v-card-actions>
-            </div>
+                </v-card>
+            </v-dialog>
         </v-card>
     </v-col>
 </template>
@@ -66,6 +95,7 @@ export default {
             term: this.dailyPosts.term,
             video: this.dailyPosts.video,
             photo: this.dailyPosts.photo,
+            dialog: false
         }
     }
 }

9d9acfa25118d4da03aea476b5950b6828e2358a
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2021-09-27 12:23:22 +0500

    Fix DailyPostsComposer.php

--- app/Http/View/Composers/DailyPostsComposer.php
+++ app/Http/View/Composers/DailyPostsComposer.php
@@ -3,6 +3,7 @@
 
 namespace App\Http\View\Composers;
 
+use App\Author;
 use App\DailyPost;
 use App\Quote;
 use App\Term;
@@ -15,15 +16,17 @@ class DailyPostsComposer
 {
     public function compose(View $view)
     {
+        // Get all daily posts
         $dailyPosts = DailyPost::where('date', Carbon::parse()
             ->format('Y-m-d'))
             ->with('quote.author')
             ->first();
-
-        if(!$dailyPosts) {
+        // If the number of post is zero
+        if (!$dailyPosts) {
+            // Create posts and get them
             $dailyPosts = $this->createDailyPosts();
         }
-
+        // Share posts variable with view instance
         $view->with('dailyPosts', $dailyPosts);
     }
 
@@ -34,17 +37,23 @@ class DailyPostsComposer
 
     private function createDailyPosts()
     {
+        // Make empty array to collect all 3 items
+        // $dailyPosts = collect([]);
         $quote = Quote::inRandomOrder()->first();
         $term = Term::inRandomOrder()->first();
         $video = Video::inRandomOrder()->first();
         $photo = Photo::inRandomOrder()->first();
 
-        return DailyPost::create([
+        // Create post
+        $dailyPosts = DailyPost::create([
             'date' => Carbon::today()->format('Y-m-d'),
             'quote_id' => $quote->id,
             'term_id' => $term->id,
             'video_id' => $video->id,
-            'photo_id' => $photo->id
+            'photo_id' => $photo->id,
         ]);
+
+        // Return all posts
+        return $dailyPosts->load('quote', 'term', 'video', 'photo', 'quote.author');
     }
 }

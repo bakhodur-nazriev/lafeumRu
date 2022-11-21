8de02fddc2dd89e17a6ce360cb315c0538bbc271
Bakhodur <bakhodur.nazriev@gmail.com>
2022-11-21 12:10:20 +0500

    Fix url vocabulary and add pagination to getShowVocabulary

--- resources/js/views/front/ListOfChildren/ListOfVocabulary.vue
+++ resources/js/views/front/ListOfChildren/ListOfVocabulary.vue
@@ -3,7 +3,7 @@
         <v-col class="subtitle-2 pa-0 font-weight-light">
             <v-hover v-slot="{ hover }">
                 <a
-                    :href="'/' + `${path === '/vocabulary' ? item.id : item.post.id}`"
+                    :href="url"
                     target="_blank"
                     class="vocabulary-words text-decoration-none d-block"
                 >
@@ -28,9 +28,14 @@ export default {
     props: ["item"],
     data() {
         return {
-            path: `${window.location.pathname}`
+            path: `${window.location.pathname}`,
         }
     },
+    computed: {
+        url() {
+            return '/' + `${window.location.pathname === '/vocabulary' ? this.item.id : this.item.post.id}`;
+        }
+    }
 }
 </script>
 

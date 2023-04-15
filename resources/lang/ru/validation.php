b4126cc4c6a11b63c8e1c987047d237516ace508
Bakhodur <bakhodur.nazriev@gmail.com>
2023-04-15 15:22:09 +0500

    Add ru lang to recourses validation.php

---
+++ resources/lang/ru/validation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'required' => 'Поле Имя и Фамилия обязательно.',
+    'email' => 'The :attribute Ova ova.',
+//    "name.required" => "Поле Имя и Фамилия обязательно.",
+//    "email.required" => "Поле электронной почты обязательно.",
+//    "email.unique" => "Электронная почта уже занята.",
+//    "password.required" => "Поле пароль обязательно.",
+//    "password_confirmation.same" => "Ova ova.",
+];
